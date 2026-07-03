4a42c3677c4cf0ea636f9d8ec9fb173cffff1084
Shiva krishna Dasari <shivakrishna0504@gmail.com>
2026-07-03 20:14:39 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        int mid=0;
+        while(mid<=end){
+            if(nums[mid]==0){
+                swap(nums,start,mid);
+                start++;
+                mid++;
+
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,end,mid);
+                end--;
+
+            }
+        }
+    }
+    public void swap(int[] nums,int a , int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+}
